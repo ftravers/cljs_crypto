0b6d2ec270786405f55ee708666d523f43ccdf0c
Fenton Travers <fenton.travers@gmail.com>
2019-03-01 14:14:08 -0800

    basic-css

--- src/cljs_crypto/core.cljs
+++ src/cljs_crypto/core.cljs
@@ -1,10 +1,18 @@
 (ns cljs-crypto.core
   (:require
+   [garden.core :refer [css]]
    [rum.core :refer [defc mount] :as rum]))
+
+(def my-style
+  [[:.my-1st-class
+    {:font-size "2em"}]])
 
 (defc hello-world-component
   []
-  [:h1 "Hello my friendly, lovely world!"])
+  [:div
+   [:style (css my-style)]
+   [:p {:class :my-1st-class}
+    "Hello lovely world!"]])
 
 (defn main-page []
   (mount
